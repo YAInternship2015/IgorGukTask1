0972cf492f980ea771f8f28e585875181f7654a5
Igor Guk <igor_guk@ua.fm>
2015-08-08 12:36:58 +0300

    second commit

---
+++ SuperHeroes/IGTableCell.m
@@ -0,0 +1,13 @@
+//
+//  IGTableCell.m
+//  SuperHeroes
+//
+//  Created by Igor Guk on 08.08.15.
+//  Copyright (c) 2015 Igor Guk. All rights reserved.
+//
+
+#import "IGTableCell.h"
+
+@implementation IGTableCell
+
+@end
--- SuperHeroes/IGTableViewController.m
+++ SuperHeroes/IGTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "IGTableViewController.h"
+#import "IGTableCell.h"
 
 @interface IGTableViewController ()
+
+@property (nonatomic, strong) NSArray *name;
+@property (nonatomic, strong) NSArray *images;
 
 @end
 
@@ -34,18 +38,7 @@
                   @"7. Harry Potter",
                   @"8. Terminator",
                   @"9. Hellboy",
-                  @"10. Hulk"];
-    
-    self.images = @[@"Yalantis2.png",
-                    @"superman1.jpg",
-                    @"spider-man1.jpg",
-                    @"santa1.png",
-                    @"batman1.jpg",
-                    @"robocop1.jpg",
-                    @"harry potter1.jpg",
-                    @"Terminator1.jpg",
-                    @"hellboy1.jpg",
-                    @"hulk1.jpg"];
+                  @"10. Hulk",];
     
 }
 
@@ -53,72 +46,26 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return 0;
+    return 10;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    // Configure the cell...
+    static NSString *cellIdentifaer = @"TableCell";
+    
+    IGTableCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifaer forIndexPath:indexPath];
+    
+    NSInteger row = [indexPath row];
+    
+    cell.nameLabel.text = self.name[row];
+    cell.thumbImage.image = [UIImage imageNamed:self.images[row]];
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
