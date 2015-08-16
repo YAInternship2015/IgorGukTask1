a37be04e0bd9bf2e6e5dbd36bd2de9c9dcde8b4c
Igor Guk <igor_guk@ua.fm>
2015-08-16 22:23:43 +0300

    editing code

--- SuperHeroes/Controllers/IGTableViewController.m
+++ SuperHeroes/Controllers/IGTableViewController.m
@@ -8,11 +8,11 @@
 
 #import "IGTableViewController.h"
 #import "IGTableCell.h"
+#import "IGHeroDataSource.h"
 
 @interface IGTableViewController ()
 
-@property (nonatomic, strong) NSArray *name;
-@property (nonatomic, strong) NSArray *images;
+@property (nonatomic, strong) IGHeroDataSource *hero;
 
 @end
 
@@ -26,58 +26,33 @@
     self.tableView.contentInset = inset;
     self.tableView.scrollIndicatorInsets = inset;
     
-    self.name = @[@"1. Yalantis-Man",
-                  @"2. Superman",
-                  @"3. Spider-Man",
-                  @"4. Santa",
-                  @"5. Batman",
-                  @"6. Robocop",
-                  @"7. Harry Potter",
-                  @"8. Terminator",
-                  @"9. Hellboy",
-                  @"10. Hulk",];
-    
-    self.images = @[@"Yalantis2.png",
-                    @"superman1.jpg",
-                    @"spider-man1.jpg",
-                    @"santa1.png",
-                    @"batman1.jpg",
-                    @"robocop1.jpg",
-                    @"harry potter1.jpg",
-                    @"Terminator1.jpg",
-                    @"hellboy1.jpg",
-                    @"hulk1.jpg",];
-    
+    self.hero = [[IGHeroDataSource alloc] init];
 }
 
 #pragma mark - TableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-
-    return self.name.count;
+    return self.hero.nameArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
     static NSString *cellIdentifaer = @"TableCell";
     
     IGTableCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifaer forIndexPath:indexPath];
     
     NSInteger row = [indexPath row];
     
-    cell.nameLabel.text = self.name[row];
-    cell.thumbImage.image = [UIImage imageNamed:self.images[row]];
+    cell.nameLabel.text = [self.hero.nameArray objectAtIndex:row];;
+    cell.thumbImage.image = [UIImage imageNamed:self.hero.imageArray[row]];
     
     return cell;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    
     return @"RATING SUPER HEROES";
 }
 
---
+++ SuperHeroes/Models/IGHeroDataSource.h
@@ -0,0 +1,16 @@
+//
+//  IGHeroDataSource.h
+//  SuperHeroes
+//
+//  Created by Igor Guk on 11.08.15.
+//  Copyright (c) 2015 Igor Guk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IGHeroDataSource : NSObject
+
+@property (nonatomic, strong) NSArray *nameArray;
+@property (nonatomic, strong) NSArray *imageArray;
+
+@end
