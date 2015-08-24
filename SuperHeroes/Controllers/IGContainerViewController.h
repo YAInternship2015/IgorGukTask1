c6ce1e4165bcba0f5a585517f7602c4d871e8049
Igor Guk <igor_guk@ua.fm>
2015-08-24 21:33:42 +0300

    add containerViewController

---
+++ SuperHeroes/Controllers/IGContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGContainerViewController.h
+//  SuperHeroes
+//
+//  Created by Igor Guk on 24.08.15.
+//  Copyright (c) 2015 Igor Guk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGContainerViewController : UIViewController
+
+@end
---
+++ SuperHeroes/Controllers/IGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGViewController.h
+//  SuperHeroes
+//
+//  Created by Igor Guk on 24.08.15.
+//  Copyright (c) 2015 Igor Guk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGViewController : UIViewController
+
+@end
